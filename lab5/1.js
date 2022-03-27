eaa28392f80b6a82406414ba2d797cb5680fd3fb
Timofey Chichikin <72801901+ned8800@users.noreply.github.com>
2022-03-27 23:53:05 +0300

    lab5

--- lab5/1.js
+++ lab5/1.js
@@ -6,3 +6,8 @@
  * [1,2,3].myMap((x) => x*2) -> [2,4,6]
  * Нужно назвать myMap !!!!!
  */
+
+Array.prototype.myMap = function (ret) {
+    return this.reduce((arr, curr) => arr.concat(ret(curr)), []);
+}
+
